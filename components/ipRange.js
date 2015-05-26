d52852f39e276914b123f92d84b2f14da2842720
krg7880 <kirkgordon7880@gmail.com>
2015-05-26 19:54:56 -0400

    Initial commit

--- components/ipRange.js
+++ components/ipRange.js
@@ -1,5 +1,11 @@
 var noflo = require('noflo');
 
+/**
+ * Returns the IP range (start & end)
+ * for a given IP/CIDR
+ * @param ip
+ * @returns {Object}
+ */
 exports.getComponent = function () {
   var c = new noflo.Component();
 
@@ -7,33 +13,25 @@ exports.getComponent = function () {
     if (event !== 'data') {
       return;
     }
+    
+    if ((payload.indexOf('/')) < 0) {
+      return c.outPorts.error.send(new Error('Invalid ips'));
+    }
+    
+    var range = {};
+    
+    var parts = payload.split('/');
+    
+    if (parts[1] > 32) {
+      return c.outPorts.error.send(new Error('Invalid ip'));
+    }
+    
+    range.start = long2ip((ip2long(parts[0])) & ((-1 << (32 - +parts[1]))));
+    range.end = long2ip((ip2long(parts[0])) + math.pow(2, (32 - +parts[1])) - 1); 
+    
     // Do something with the packet, then
-    c.outPorts.out.send(payload);
+    c.outPorts.out.send(range);
   });
   c.outPorts.add('out');
   return c;
-};
-
-/**
- * Returns the IP range (start & end)
- * for a given IP/CIDR
- * @param ip
- * @returns {Object}
- */
-CIDR.prototype.range = function(ip) {
-    if (!(ip.indexOf('/') > -1)) {
-        return null;
-    }
-
-    var range = {};
-    var parts = ip.split('/');
-
-    if ((parts[1] > 32)) {
-        return null;
-    }
-
-    range.start = long2ip((ip2long(parts[0])) & ((-1 << (32 - +parts[1]))));
-    range.end = long2ip((ip2long(parts[0])) + math.pow(2, (32 - +parts[1])) - 1);
-
-    return range;
 };
