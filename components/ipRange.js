e939cb835e0682f4bd8a9d0f32bd67b383a08cef
krg7880 <kirkgordon7880@gmail.com>
2015-05-26 20:31:23 -0400

    initial commit

--- components/ipRange.js
+++ components/ipRange.js
@@ -1,5 +1,6 @@
 var noflo = require('noflo');
 var long2ip = require('long2ip');
+var noflo = require("noflo");
 
 /**
  * Returns the IP range (start & end)
@@ -7,35 +8,35 @@ var long2ip = require('long2ip');
  * @param ip
  * @returns {Object}
  */
-exports.getComponent = function () {
-  var c = new noflo.Component();
+exports.getComponent = function() {
+  var component = new noflo.Component();
+  component.description = "Filters the given IP into CIDR blocks";
 
-  c.inPorts.add('in', function (event, payload) {
-    if (event !== 'data') {
-      return;
+  // Register ports and event handlers
+  component.inPorts.add('in', { datatype: 'array' }, function(event, payload) {
+    switch (event) {
+      case 'data':
+        if ((payload.indexOf('/')) < 0) {
+          return c.outPorts.error.send(new Error('Invalid ips'));
+        }
+    
+        var range = {};
+    
+        var parts = payload.split('/');
+    
+        if (parts[1] > 32) {
+          return c.outPorts.error.send(new Error('Invalid ip'));
+        }
+    
+        range.start = long2ip((ip2long(parts[0])) & ((-1 << (32 - +parts[1]))));
+        range.end = long2ip((ip2long(parts[0])) + math.pow(2, (32 - +parts[1])) - 1);
+        return component.outPorts.out.send(payload);
+      case 'disconnect':
+        // Disconnect output port when input port disconnects
+        return component.outPorts.out.disconnect();
     }
-    
-    if ((payload.indexOf('/')) < 0) {
-      console.log('Sending out out');
-      return c.outPorts.out.send(new Error('Invalid ips'));
-    }
-    
-    var range = {};
-    
-    var parts = payload.split('/');
-    
-    if (parts[1] > 32) {
-      console.log('Sending out out');
-      return c.outPorts.out.send(new Error('Invalid ip'));
-    }
-    
-    range.start = long2ip((ip2long(parts[0])) & ((-1 << (32 - +parts[1]))));
-    range.end = long2ip((ip2long(parts[0])) + math.pow(2, (32 - +parts[1])) - 1); 
-    
-    // Do something with the packet, then
-    console.log('Sending out out 3');
-    c.outPorts.out.send(range);
   });
-  c.outPorts.add('out');
-  return c;
+  component.outPorts.add('out', { datatype: 'all' });
+
+  return component; // Return new instance
 };
