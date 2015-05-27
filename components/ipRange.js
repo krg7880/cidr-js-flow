72c82d57fc5350d65fa88e96be00dc4935d81ce2
krg7880 <kirkgordon7880@gmail.com>
2015-05-27 09:52:18 -0400

    initial commit

--- components/ipRange.js
+++ components/ipRange.js
@@ -1,35 +1,30 @@
 var long2ip = require('long2ip');
 var noflo = require('noflo');
+
 exports.getComponent = function() {
   var component = new noflo.Component();
-  component.description = "Filters the given IP into CIDR blocks";
+  component.description = "Filter the given IPs into CIDR blocks";
 
-  component.inPorts = {
-    in: new noflo.Port('array')
-  };
+  component.inPorts.add('in', {datatype: 'array'}, function(e, payload) {
+    switch(e) {
+      case 'data':
+        if ((payload.indexOf('/')) < 0) {
+          return component.outPorts.error.send(new Error('Invalid ips'));
+        }
 
-  component.outPorts = {
-    out: new noflo.Port('object'),
-    error: new noflo.Port('object')
-  };
+        var range = {};
 
-  component.inPorts.in.on('data', function(payload) {
-    if ((payload.indexOf('/')) < 0) {
-      return component.outPorts.error.send(new Error('Invalid ips'));
+        var parts = payload.split('/');
+
+        if (parts[1] > 32) {
+          return component.outPorts.error.send(new Error('Invalid ip'));
+        }
+
+        range.start = long2ip((ip2long(parts[0])) & ((-1 << (32 - +parts[1]))));
+        range.end = long2ip((ip2long(parts[0])) + math.pow(2, (32 - +parts[1])) - 1);
+        return component.outPorts.out.send(payload);
     }
-
-    var range = {};
-
-    var parts = payload.split('/');
-
-    if (parts[1] > 32) {
-      return component.outPorts.error.send(new Error('Invalid ip'));
-    }
-
-    range.start = long2ip((ip2long(parts[0])) & ((-1 << (32 - +parts[1]))));
-    range.end = long2ip((ip2long(parts[0])) + math.pow(2, (32 - +parts[1])) - 1);
-    return component.outPorts.out.send(payload);
   });
-
-  return component; // Return new instance
+  
+  return component;
 };
